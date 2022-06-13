c2637e773f33d32117bf300fce747e336e1f3e26
Vincent Sit <vincentsit00@ymail.com>
2022-06-13 21:13:45 +1200

    Update methods

--- src/com/company/Hand.java
+++ src/com/company/Hand.java
@@ -1,21 +1,27 @@
 package com.company;
 
-public class Hand extends CardCollection{
+public class Hand extends CardCollection implements CardsRemovable{
 
     public Hand() {
         super();
     }
 
-    public Card dropCard(int cardPosition) {
+    public Card dropCard() throws ArrayIndexOutOfBoundsException {
+        if (cards.size() > 0) {
+            return cards.remove(cards.size() - 1);
+        } else {
+            throw new ArrayIndexOutOfBoundsException("There are no cards to drop.");
+        }
+    }
+
+    public Card dropCard(int cardPosition) throws ArrayIndexOutOfBoundsException {
         return cards.remove(cardPosition);
     }
 
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder("Cards in hand" + ":\n");
-        for(Card card : cards) {
-            s.append(card.toString()).append("\n");
-        }
+        s.append(super.toString());
         return s.toString();
     }
 }
