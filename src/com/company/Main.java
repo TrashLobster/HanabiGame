234fe76f70ce0206e62a1733743a552d8ba1aac6
Vincent Sit <vincentsit00@ymail.com>
2022-06-15 00:11:59 +1200

    Further clean up of main code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,26 +19,27 @@ public class Main {
 
         List<Player> players = playerList.getPlayers();
 
-        for (int i; i<players.size(); i++) {
+        int playerSize = playerList.getNumberOfPlayers();
+
+        // TODO: turn PlayerList List<Player> into an iterable
+        for (int i; i<playerSize; i++) {
             System.out.println(players.get(i).getName() + " : Order of Play " + players.get(i).getOrderOfPlay());
         }
 
-        CardDeck deck = game.getCardDeck();
+        CardDeck shuffledDeck = game.getCardDeck();
         CardDeck discardPile = game.getDiscardPile();
 
-        for (Player player : playerList) {
-            int handHasCards = 4;
-            if (playerList.size() == 2 || playerList.size() == 3) {
-                handHasCards = 5;
+        for (int i; i<playerSize; i++) {
+            int handHasCards = playerSize == 2 || playerSize == 3 ? 5 : 4;
+
+            for (int j = handHasCards; j>0; j--) {
+                players.get(i).addCard(shuffledDeck.dropCard(0));
             }
-            while (handHasCards != 0) {
-                player.addCard(deck.dropCard(0));
-                handHasCards--;
-            }
+
         }
 
         boolean gameOn = true;
-        int countdownOfRounds = playerList.size() + 1;
+        int countdownOfRounds = playerSize + 1;
         int turn = 0;
         while (gameOn) {
             turn++;
@@ -87,10 +88,10 @@ public class Main {
                                     turnOver = giveHint(playerList, listOfPlayerNames, player, noteTokens);
                                     break;
                                 case 7:
-                                    turnOver = discardCard(player, discardPile, noteTokens, deck);
+                                    turnOver = discardCard(player, discardPile, noteTokens, shuffledDeck);
                                     break;
                                 case 8:
-                                    turnOver = playCard(player, fireworkCollection, discardPile, deck, noteTokens,
+                                    turnOver = playCard(player, fireworkCollection, discardPile, shuffledDeck, noteTokens,
                                             stormTokens);
                                     break;
                                 case 9:
@@ -112,7 +113,7 @@ public class Main {
                     score += firework.getNextValueExpected() - 1;
                 }
 
-                if (deck.size() == 0) {
+                if (shuffledDeck.size() == 0) {
                     System.out.println("There are no cards left in the deck! Each player has one more turn.");
                     countdownOfRounds--;
                 }
@@ -124,54 +125,60 @@ public class Main {
                         score = 0;
                     }
                     System.out.println("\nYour final score is " + score + ". And...");
-                    switch (score) {
-                        case 0:
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                        case 5:
-                            System.out.println(ANSI_BLACK + "Oh dear! The crowd booed.");
-                            break;
-                        case 6:
-                        case 7:
-                        case 8:
-                        case 9:
-                        case 10:
-                            System.out.println(ANSI_PURPLE + "Poor! A smattering of applause.");
-                            break;
-                        case 11:
-                        case 12:
-                        case 13:
-                        case 14:
-                        case 15:
-                            System.out.println(ANSI_CYAN + "OK! The viewers have seen better.");
-                            break;
-                        case 16:
-                        case 17:
-                        case 18:
-                        case 19:
-                        case 20:
-                            System.out.println(ANSI_BLUE + "Good! The audience is pleased.");
-                            break;
-                        case 21:
-                        case 22:
-                        case 23:
-                        case 24:
-                            System.out.println(ANSI_GREEN + "Very good! The audience is enthusiastic!");
-                            break;
-                        case 25:
-                            System.out.println(ANSI_RED + "Legendary! The audience will never forget this show!");
-                            break;
-                        default:
-                            break;
-                    }
+                    
+                    finalResultAnnouncement(score);
+                    
                     System.out.println(ANSI_RESET + "\nGame over. Thanks for playing!");
 
                     gameOn = false;
                     break;
                 }
             }
+        }
+    }
+
+    public static void finalResultAnnouncement(int score) {
+        switch (score) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(ANSI_BLACK + "Oh dear! The crowd booed.");
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println(ANSI_PURPLE + "Poor! A smattering of applause.");
+                break;
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+                System.out.println(ANSI_CYAN + "OK! The viewers have seen better.");
+                break;
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+                System.out.println(ANSI_BLUE + "Good! The audience is pleased.");
+                break;
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+                System.out.println(ANSI_GREEN + "Very good! The audience is enthusiastic!");
+                break;
+            case 25:
+                System.out.println(ANSI_RED + "Legendary! The audience will never forget this show!");
+                break;
+            default:
+                break;
         }
     }
 
