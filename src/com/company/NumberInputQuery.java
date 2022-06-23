e47bfab1746111618e535b74e02efde57bca5bcd
Vincent Sit <vincentsit00@ymail.com>
2022-06-24 11:58:41 +1200

    Create number input query class

---
+++ src/com/company/NumberInputQuery.java
@@ -0,0 +1,94 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class NumberInputQuery implements InputQuery{
+    private static final Scanner scan = new Scanner(System.in);
+    private String inputReceived;
+    private int upperBound;
+    private int lowerBound;
+    private boolean isEntryANumber;
+    private boolean isNumberAcceptable;
+
+    public NumberInputQuery(int upperBound, int lowerBound) {
+        this.inputReceived = "";
+        this.upperBound = upperBound;
+        this.lowerBound = lowerBound;
+        checkIfInputIsNumber();
+    }
+
+    public String getInputReceived() {
+        return inputReceived;
+    }
+
+    public int getUpperBound() {
+        return upperBound;
+    }
+
+    public int getLowerBound() {
+        return lowerBound;
+    }
+
+    public boolean getIsEntryANumber() {
+        return isEntryANumber;
+    }
+
+    public boolean getIsNumberAcceptable() {
+        return isNumberAcceptable;
+    }
+        
+    public int convertEntryToInteger() {
+        return Integer.parseInt(inputReceived);
+    }
+
+    public void setInputReceived(String newInput) {
+        inputReceived = newInput;
+    }
+
+    public void confirmEntryAsNumber() {
+        this.isEntryANumber = true;
+    }
+
+    public void accceptNumber() {
+        this.isNumberAcceptable = true;
+    }
+    
+    public void runInputQuery() {
+       
+        while(!isNumberAcceptable) {
+            checkIfInputIsNumber();
+            if (isNumberWithinBounds()) {
+                accceptNumber();
+            } else {
+                System.out.println("Please try another number");
+                enterInput();
+            }
+        }
+    }
+
+    public void checkIfInputIsNumber() {
+        while(!isEntryANumber) {
+            System.out.println("Please enter a number:");
+            enterInput();
+            if (isEntryCorrectType()) {
+                confirmEntryAsNumber();
+
+            } else {
+                System.out.println("Entry is not a number. Please try again:");
+            }
+        }
+    }
+
+    public boolean isEntryCorrectType() {
+        return inputReceived.matches("[0-9]+");
+    }
+
+    public void enterInput() {
+        setInputReceived(scan.nextLine());
+    }
+
+    public boolean isNumberWithinBounds() {
+        return convertEntryToInteger() >= lowerBound && convertEntryToInteger() <= upperBound;
+    }
+
+}
